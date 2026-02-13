2b44c8278ae81e7acf8e001331300d2216714797
Sebas-Mendez <sebastianmendezochoa92@gmail.com>
2026-02-13 16:57:59 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Primeros comandos de git");
-        // Sebastian Mendez (Python)
+        //
+        System.out.println("Sebastian Mendez (Python)");
 
 
 
