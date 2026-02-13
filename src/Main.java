4992f3df301a14253b92f82fa17262a4512602e2
Sebas-Mendez <sebastianmendezochoa92@gmail.com>
2026-02-13 17:16:43 -0500

    Comandos Git

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Primeros comandos de git");
-        //
         System.out.println("Sebastian Mendez (Python)");
+
+        System.out.printf("1. COMANDOS DE GIT VISTOS");
+        System.out.printf("1. git add .");
+        System.out.printf("2. git commit -m");
+        System.out.printf("3. git push");
+        System.out.printf("4. git switch");
 
 
 
